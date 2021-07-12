e9201a163ea3a062a4c48159da0697c36ddac8c9
Nicke-lucky <13965882371@163.com>
2021-07-12 14:16:54 +0800

    add core-sdk-go

---
+++ client/grpc_client.go
@@ -0,0 +1,45 @@
+package client
+
+import (
+	"sync"
+	"time"
+
+	"github.com/prometheus/common/log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
+
+	"github.com/irisnet/core-sdk-go/types"
+)
+
+var clientConnSingleton *grpc.ClientConn
+var once sync.Once
+
+type grpcClient struct {
+}
+
+func NewGRPCClient(url string) types.GRPCClient {
+	once.Do(func() {
+		var kacp = keepalive.ClientParameters{
+			Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
+			Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead
+			PermitWithoutStream: true,             // send pings even without active streams
+		}
+
+		dialOpts := []grpc.DialOption{
+			grpc.WithInsecure(),
+			grpc.WithKeepaliveParams(kacp),
+		}
+		clientConn, err := grpc.Dial(url, dialOpts...)
+		if err != nil {
+			log.Error(err.Error())
+			panic(err)
+		}
+		clientConnSingleton = clientConn
+	})
+
+	return &grpcClient{}
+}
+
+func (g grpcClient) GenConn() (*grpc.ClientConn, error) {
+	return clientConnSingleton, nil
+}
