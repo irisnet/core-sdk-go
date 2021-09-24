0427a85a8f70e2c52ffb95bc0fd3908f41470e59
corey_zhang <nicholouszhang@163.com>
2021-09-24 15:13:48 +0800

    add encodingconfig

---
+++ types/codec.go
@@ -0,0 +1,14 @@
+package types
+
+import (
+	"github.com/irisnet/core-sdk-go/codec"
+	"github.com/irisnet/core-sdk-go/codec/types"
+)
+
+// EncodingConfig specifies the concrete encoding types to use for a given app.
+// This is provided for compatibility between protobuf and amino implementations.
+type EncodingConfig struct {
+	InterfaceRegistry types.InterfaceRegistry
+	Marshaler         codec.Codec
+	TxConfig          TxConfig
+}
