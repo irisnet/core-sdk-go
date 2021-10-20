8973ff87203c74b5978ebc8c175facdb99cfa6c0
corey_zhang <nicholouszhang@163.com>
2021-10-20 17:35:43 +0800

    fix

--- modules/bank/errors.go
+++ modules/bank/errors.go
@@ -8,8 +8,8 @@ const Codespace = ModuleName
 
 var (
 	ErrQueryAccount     = errors.Register(Codespace, 1, "query account")
-	ErrQueryTotalSupply = errors.Register(Codespace, 1, "query total supply option")
-	ErrBuildAndSend     = errors.Register(Codespace, 7, "BuildAndSend error")
-	ErrToMinCoin        = errors.Register(Codespace, 22, "ToMinCoin error")
-	ErrGenConn          = errors.Register(Codespace, 24, "generate conn error")
+	ErrQueryTotalSupply = errors.Register(Codespace, 2, "query total supply option")
+	ErrBuildAndSend     = errors.Register(Codespace, 3, "BuildAndSend error")
+	ErrToMinCoin        = errors.Register(Codespace, 4, "ToMinCoin error")
+	ErrGenConn          = errors.Register(Codespace, 5, "generate conn error")
 )
