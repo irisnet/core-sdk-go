6e5db976eac224ef72c3fee03ad06f51ed3e01e9
corey_zhang <nicholouszhang@163.com>
2021-10-28 17:21:29 +0800

    fix

--- modules/bank/export.go
+++ modules/bank/export.go
@@ -9,7 +9,9 @@ import (
 // expose bank module api for user
 type Client interface {
 	types.Module
+	// Send defines a method for sending coins from one account to another account.
 	Send(to string, amount types.DecCoins, baseTx types.BaseTx) (ctypes.ResultTx, error)
+	// MultiSend defines a method for sending coins from some accounts to other accounts.
 	SendWitchSpecAccountInfo(to string, sequence, accountNumber uint64, amount types.DecCoins, baseTx types.BaseTx) (ctypes.ResultTx, error)
 	MultiSend(receipts MultiSendRequest, baseTx types.BaseTx) ([]ctypes.ResultTx, error)
 	SubscribeSendTx(from, to string, callback EventMsgSendCallback) types.Subscription
