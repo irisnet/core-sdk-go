79f28dc27415547ca37095f8544e4ffecb19bc40
孙 莹波 <yingbo@bianjie.ai>
2022-07-05 14:44:29 +0800

    format_page_request add return error msg

--- types/query/format_page_request.go
+++ types/query/format_page_request.go
@@ -1,9 +1,32 @@
 package query
 
-func FormatPageRequest(pageReq *PageRequest) *PageRequest {
+import sdk "github.com/irisnet/core-sdk-go/types"
+
+const (
+	CountTotalErrMsg      = "pageRequest error: CountTotal must be false"
+	LimitErrMsg           = "pageRequest error: Limit cannot be empty and cannot exceed 100"
+	CountTotalLimitErrMsg = "pageRequest error: CountTotal must be false and Limit cannot be empty and cannot exceed 100"
+)
+
+func FormatPageRequest(pageReq *PageRequest) (*PageRequest, sdk.Error) {
+	var msg string
+	if pageReq.CountTotal {
+		msg = CountTotalErrMsg
+	}
 	pageReq.CountTotal = false
 	if pageReq.Limit == 0 || pageReq.Limit > 100 {
 		pageReq.Limit = 100
+		if msg != "" {
+			msg = CountTotalLimitErrMsg
+		} else {
+			msg = LimitErrMsg
+		}
 	}
-	return pageReq
+
+	var err sdk.Error
+	if msg != "" {
+		err = sdk.Wrapf(msg)
+	}
+
+	return pageReq, err
 }
