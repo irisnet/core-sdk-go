82f712728bd890d9e8349dacd5114c3a0b77b823
孙 莹波 <yingbo@bianjie.ai>
2022-07-20 16:16:17 +0800

    pageRequest nil judge

--- types/query/format_page_request.go
+++ types/query/format_page_request.go
@@ -3,11 +3,15 @@ package query
 import sdk "github.com/irisnet/core-sdk-go/types"
 
 const (
+	PageReqNilErrMsg = "pageRequest error: PageRequest cannot be nil"
 	CountTotalErrMsg = "pageRequest error: CountTotal is not supported, must be false"
 	LimitErrMsg      = "pageRequest error: Limit cannot be empty and cannot exceed 100"
 )
 
 func FormatPageRequest(pageReq *PageRequest) (*PageRequest, sdk.Error) {
+	if pageReq == nil {
+		return pageReq, sdk.Wrapf(PageReqNilErrMsg)
+	}
 	if pageReq.CountTotal {
 		return pageReq, sdk.Wrapf(CountTotalErrMsg)
 	}
